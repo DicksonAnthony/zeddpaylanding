510d99511ff23dc3859e2a07a381645882d8f92d
DicksonAnthony <huntaba6@gmail.com>
2022-07-15 16:11:32 +0100

    modified:   src/components/Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <>
+      <Wrapper>
+        <Leftdiv>
+          <Image />
+          <h1></h1>
+        </Leftdiv>
+        <MiddleDiv>
+          <Ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact</li>
+            <li>Rates</li>
+          </Ul>
+        </MiddleDiv>
+        <Rightdiv>
+          <ButtonL>Log in</ButtonL>
+          <ButtonG>Get Started</ButtonG>
+        </Rightdiv>
+      </Wrapper>
+    </>
+  );
+};
 
-export default Nav
+export default Nav;
