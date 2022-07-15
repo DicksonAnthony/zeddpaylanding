bb78fa064fa1b2ae90169c3ddb815996059cbda4
DicksonAnthony <huntaba6@gmail.com>
2022-07-15 16:01:49 +0100

    Arranging files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Accordian from "./components/Accordian";
+import Contents from "./components/Contents";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { GlobalStyle } from "./GlobalStyle";
+function App() {
+  return (
+    <>
+      <Nav />
+      <Contents />
+      <Accordian />
+      <Footer />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --buttonText: #fff;
+        --navColor: #fafafa;
+        --headingColor: #121212;
+        --primaryColor: #007de8;
+        --paragraphColor: rgba(0,25,46,.5)!important;
+        --doodleHeader: url(https://zeddpay.com/svg/heroTop.svg);
+        --doodleSection: url(https://zeddpay.com/svg/heroDoodle.svg);
+        --bitMogi: url(https://zeddpay.com/_next/static/media/woman.fc21ff3a.svg);
+        --backgroundGradient: linear-gradient(161.48deg,#007de8 -47.77%,rgba(0,232,190,0) 146.51%);
+    }
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Poppins", sans-serif;
+    }
+`;
