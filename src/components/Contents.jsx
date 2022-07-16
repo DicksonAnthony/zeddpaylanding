88e9d06d7ce79f62bd54eadfeaf271da78c3a8d0
DicksonAnthony <huntaba6@gmail.com>
2022-07-16 12:28:01 +0100

    .

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Wrapper, Cards } from "./styles/Contents.styled";
+import { Wrapper, Cards, Desc } from "./styles/Contents.styled";
 
 const Contents = () => {
   return (
     <Wrapper>
-      <Cards>hello</Cards>
-      <Cards>hello</Cards>
-      <Cards>hello</Cards>
-      <Cards>hello</Cards>
-      <Cards>hello</Cards>
+      <Cards>
+        <h1>Purchase anything online with your airtime</h1>
+        <Desc>
+          Shop online, go premuim, get assets, pay for services and most
+          importantly purchase that new Call of Duty weapon with airtime
+        </Desc>
+      </Cards>
+      <Cards>
+        <h1></h1>
+        <Desc></Desc>
+      </Cards>
     </Wrapper>
   );
 };
